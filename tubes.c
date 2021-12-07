d5daf978e8f392a994d4d651db075763b706973d
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-07 09:40:48 +0800

    up file

---
+++ tubes.c
@@ -0,0 +1,22 @@
+#include "function.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void header (){
+    system ("cls");
+	printf  ("\n\n\t\t\t\t\t=============================================\n");
+	printf  ("\t\t\t\t\t||         Program Catatan Keuangan        ||\n");
+	printf  ("\t\t\t\t\t============================================\n");
+	printf  ("\t\t\t\t\t||     Program dibuat dan disusun oleh:    ||\n");
+	printf  ("\t\t\t\t\t||     --------------------------------    ||\n");
+	printf  ("\t\t\t\t\t||              Dwi Tirta Wati             ||\n");
+	printf  ("\t\t\t\t\t||                    &                    ||\n");
+	printf  ("\t\t\t\t\t||         Leony Mona Putri Sihaloho       ||\n");
+	printf  ("\t\t\t\t\t=============================================\n");
+	printf  ("\t\t\t\t\t       Tekan ENTER  untuk melanjutkan....    \n");
+    printf  ("\t\t\t\t\t=============================================\n");
+	getchar ();
+    system ("cls");
+
+}
