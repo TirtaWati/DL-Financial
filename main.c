b45fde3e398eecdca409003de99621b274774413
leonyhlh <94913391+leonyhlh@users.noreply.github.com>
2021-12-07 09:44:08 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+//Header yang di gunakan untuk standar input output pada bahasa C
+#include <stdlib.h>
+#include "login.c"
+#include "tubes.c"
+#include "display.c"
+
+// fungsi main () adalah kepala dari program ini, dimana program akan di eksekusi oleh fungsi main ini.
+int main (){
+    header();
+    system ("clear");
+    menu_masuk();
+}
