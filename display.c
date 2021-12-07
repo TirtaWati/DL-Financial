656258be3ee135dcc435ccd2da9772fac9bc2a3f
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-07 09:50:38 +0800

    up file display.c

---
+++ display.c
@@ -0,0 +1,10 @@
+#include "variable.h"
+#include "structure.h"
+#include "function.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void error_alert(){
+    printf   ("\n\n\t\t\t\t\t||            Pilihan Tidak Ada!!!           || \n");
+    printf   ("\t\t\t\t\t||       Silahkan Pilih Pilihan yang Ada     || \n");
+}
