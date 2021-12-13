d137e42830c8b73cd51aba1afb92d10fc456da6c
Leony Mona Putri Sihaloho <94913391+leonyhlh@users.noreply.github.com>
2021-12-13 23:21:09 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 //Header yang di gunakan untuk standar input output pada bahasa C
 #include <stdlib.h>
 #include "login.c"
-#include "tubes.c"
 #include "menu.c"
+#include "display.c"
 
 // fungsi main () adalah kepala dari program ini, dimana program akan di eksekusi oleh fungsi main ini.
 int main (){
