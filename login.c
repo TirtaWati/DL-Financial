5dc3a52d3f861141d8c4a7b5abd6d761be910f81
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-16 00:37:05 +0800

    Update login.c

--- login.c
+++ login.c
@@ -1,14 +1,40 @@
+/***********************************************************|
+|                    LOGIN & REGISTRASI                     |
+|TANGGAL PEMBUATAN : 7 DESEMBER 2021                        |
+|OLEH : DWI TIRTA WATI                                      |
+|DESKRIPSI : REVISI FUNGSI UNTUK MELAKUKAN REGISTRASI DAN   |
+|           lOGIN PENGGUNA                                  |
+|REVISI : 1                                                 |
+|DESKRIPSI : Membuat dataLog.txt menyimpan tipa data pada 1 |
+|line, dan menambahkan difine untuk membaca seluruh data di |
+|info.txt                                                   |
+************************************************************/
+/***********************************************************|
+|                    LOGIN & REGISTRASI                     |
+|TANGGAL PEMBUATAN : 14 DESEMBER 2021                       |
+|OLEH : DWI TIRTA WATI                                      |
+|DESKRIPSI : REVISI FUNGSI UNTUK MELAKUKAN REGISTRASI DAN   |
+|           lOGIN PENGGUNA                                  |
+|REVISI : 2                                                 |
+|DESKRIPSI : menambahkan file logrecord untuk menyimpan     |
+|            value usrname yang ada di login agar dapat     |
+|            melakukan strcmp di fungsi lain (infouser())   |
+************************************************************/
+
 #include "variable.h"
 #include "function.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//define buffer_size ini untuk mendeklarasikan banyak/jumlah buffer max pada file
+#define BUFFER_SIZE 1000
+
 void daftar(){
     // Membuat pointer regis untuk file "dataLog.txt"
     FILE *registrasi;
-    // Membuka file "dataLog.txt" dengan mode "w"
-    registrasi = fopen(namaFile,"w");
+    // Membuka file "dataLog.txt" dengan mode append
+    registrasi = fopen(namaFile,"a");
     // Melakukan Pengecekan apakah pointer regis menunjuk kepada file yang dituju ("dataLog.txt")
     if (registrasi == NULL)
     {
@@ -35,6 +61,7 @@ void daftar(){
 
     // Menuliskan nama yang diinput oleh pengguna kedalam file "dataLog.txt"
     fwrite(&u,sizeof(u),1,registrasi);
+    fprintf(registrasi, "\n");          //print new line agar data lain tidak berada di satu baris yang sama
     fclose(registrasi);
     getchar();
     system ("clear");
@@ -43,107 +70,72 @@ void daftar(){
 }
     
 void masuk(){
-    char username[10];
-    char password[8];
 
     // Variabel pilihan untuk pilihan user apakah ingin mendaftar atau keluar
     int pilihan;
 
-    // Membuat pointer record untuk menunjuk pada file "dataLog.txt"
-    FILE *record;
+    // Membuat pointer log untuk menunjuk pada file "dataLog.txt"
+    FILE *log, *record;
     // Membuka file dengan mode "r"
-    record = fopen (namaFile, "r");
-    if (record == NULL){
+    log = fopen (namaFile, "r");
+    if (log == NULL){
         fputs ("Tidak terdapat FILE!!\n", stderr);
         daftar();
     }
+    record = fopen("logrecord.txt", "w");           /* pointer record digunakan untuk membuka file loglog.txt dengan mode w 
+                                                     (dimana data yang tersimpan akan terus berganti setiap kali login dilakukan)*/
 
     // Arahan untuk LOGIN
     printf ("\n\n\t\t\t\t\t=====================\n");
     printf ("\t\t\t\t\t        LOGIN\n");
     printf ("\t\t\t\t\t---------------------\n");
     printf ("\t\t\t\t\t Username: ");
-    scanf  ("%s", username);
+    scanf  ("%s", &username);
     printf ("\t\t\t\t\t---------------------\n");
     printf ("\t\t\t\t\t Password: ");
-    scanf  ("%s", password);
+    scanf  ("%s", &password);
     printf ("\t\t\t\t\t--------------------\n");
     system ("clear");
-    // Membaca file "dataLog.txt"
-    while(fread(&u,sizeof(u),1,record)){
+    
+    //value usrname ditulis dan disimpan pada file logrecord
+    fwrite(&username, sizeof(username), 1, record);       
+
+    //pengecekan username yang dimasukan apakah valid dengan dataLog.txt atau tidak
+    while(fgets(&u, BUFFER_SIZE, log)){              //loop while, semua yang data dalam dataLog.txt dibaca 
+
         /*  Mengkomparasi username serta password yang diinput pengguna kedalam fungsi
             masuk() dengan username serta password yang sudah terdaftar */
-        if(strcmp(username, u.username)==0 && strcmp(password, u.password)==0){
-            printf ("\n\n\t\t\t\t\t========================\n");
-            printf ("\t\t\t\t\t     Login Berhasil!\n");
-            printf ("\t\t\t\t\t========================\n");
-
+        if(strcmp(&username, &u.username) == 0 && strcmp(&password, &u.password) == 0){      //membandingkan apakah username dan password valid
+            system("clear");
+            printf  ("\t\t\t\t\t Selamat Datang %s!!\n", &u.nama);
+            home();
         }
-        //  Jika akun tidak terdaftar maka akan diberikan pilihan untuk mendaftar atau keluar dari program
-        else {
-            printf ("\n\n\t\t\t\t\t===============================\n");
-            printf ("\t\t\t\t\t      Akun tidak terdaftar!\n");
-            printf ("\t\t\t\t\t===============================\n");
-
-            instruksi:
-            printf ("\t\t\t\t\tKetik 1 untuk daftar");
-            printf ("\n\t\t\t\t\tKetik 2 untuk masuk ulang");
-            printf ("\n\t\t\t\t\tKetik 3 untuk EXIT\n");
-            printf ("\t\t\t\t\t-------------------------------\n");
-            printf ("\t\t\t\t\tKetik Pilihan: ");
-            scanf ("%d", &pilihan);
-            system ("clear");
-            // Jika pengguna mengetik nomor 1 maka akan dialihkan kepada fungsi daftar
-            if (pilihan == 1){
-                daftar();
-            /* Menggunakan fungsi recursive kepada fungsi masuk agar pengguna
-               dapat melakukan percobaan masuk kembali jika terjadi kesalahan penulisan */
-            } else if (pilihan==2) {
-                masuk();
-            } else if (pilihan ==3){
-                exit(1);
-            } else {
-                printf ("\n\n\t\t\t\t\t ERROR!!!\n");
-                goto instruksi;
-            }
+        else if(strcmp(&username, &u.username)== 0 && strcmp(&password, &u.password) != 0 ){ //membandingkan apakah pass yang diinputkan salah
+            system("clear");
+			printf ("\n\n\t\t\t\t\t===============================\n");                      //kondisi kedua maka login gagal (karena pass salah)
+			printf ("\t\t\t\t\tPassword yang Anda Masukan Salah!\n");
+			printf ("\t\t\t\t\t===============================\n");
+            //instruksi apakah user ingin melakukan login ulang
+			printf ("Ingin Login Ulang? (Y|N): ");                
+			scanf  ("%s", &yn);                                      
+			if(strcmp(&yn, "y") == 0 || strcmp(&yn, "Y") == 0){   
+				masuk();
+			}else {
+				exit(0);
+			}
+        }
+        // selain dua kondisi diatas maka sistem mengeluarkan printf dibawah dan keluar dari program
+        //  akun tidak terdaftar maka akan diberikan pilihan untuk mendaftar atau keluar dari program
+        else {           
+            system("clear");
+            printf ("\n\n\t\t\t\t\t=============================================\n");
+            printf("\t\t\t\t\t\tUsername tidak terdaftar!\n");
+            printf("\t\t\t\t\tSilahkan melakukan registrasi terlebih dahulu");
+            printf ("\n\t\t\t\t\t=============================================\n");
         }
     }
+    /*menutup kedua file dataLog.txt dan logrecord.txt*/
+    fclose(log);            
     fclose(record);
     return;
-}
-void menu_masuk(){
-    int pilihanmasuk;
-
-    // Arahan yang diberikan kepada pengguna
-    arahan:
-    printf ("\n\n\t\t\t\t\t===============================================\n");
-    printf ("\t\t\t\t\t||                 MENU MASUK                ||\n");
-    printf ("\t\t\t\t\t||===========================================||\n");
-    printf ("\t\t\t\t\t||   No  |    Menu                           ||\n");
-    printf ("\t\t\t\t\t||-------|-----------------------------------||\n");
-    printf ("\t\t\t\t\t||   1   |    Masuk                          ||\n");
-    printf ("\t\t\t\t\t||   2   |    Registrasi                     ||\n");
-    printf ("\t\t\t\t\t||   3   |    Exit                           ||\n");
-    printf ("\t\t\t\t\t===============================================\n");
-    printf ("\t\t\t\t\tInput pilihan Anda = ");
-    scanf  ("%d", &pilihanmasuk);
-    system ("clear");
-
-    switch (pilihanmasuk){
-    case 1:
-        // Jika pengguna menginput angka 1 maka dipanggil fungsi masuk
-        masuk();
-        break;
-    case 2:
-        // Jika pengguna menginput angka 2 maka dipanggil fungsi registrasi
-        daftar();
-        break;
-    case 3:
-        exit(1);
-        break;
-    default:
-    	error_alert();
-        goto arahan;
-        break;
-    }
 }
