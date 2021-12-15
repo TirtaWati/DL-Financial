63579ed99c8c7b969c1257ecd550ff84485d4628
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-16 00:38:08 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,37 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void header (){
-    system ("cls");
-	printf  ("\n\n\t\t\t\t\t=============================================\n");
-	printf  ("\t\t\t\t\t||         Program Catatan Keuangan        ||\n");
-	printf  ("\t\t\t\t\t============================================\n");
-	printf  ("\t\t\t\t\t||     Program dibuat dan disusun oleh:    ||\n");
-	printf  ("\t\t\t\t\t||     --------------------------------    ||\n");
-	printf  ("\t\t\t\t\t||              Dwi Tirta Wati             ||\n");
-	printf  ("\t\t\t\t\t||                    &                    ||\n");
-	printf  ("\t\t\t\t\t||         Leony Mona Putri Sihaloho       ||\n");
-	printf  ("\t\t\t\t\t=============================================\n");
-	printf  ("\t\t\t\t\t        Klik ENTER untuk melanjutkan....    \n");
-    printf  ("\t\t\t\t\t=============================================\n");
-	getchar ();
-    system ("cls");
-
-}
-
 //=======================================================================//
 //************  Fungsi Untuk Menampilkan Halaman Utama   ****************//
 //=======================================================================//
-// Tgl  	  : 14-12-2021                                   	 //
-// Oleh		  : Leony Mona Putri Sihaloho - 2105551022		 //
-// Revisi	  : 0					                 //
+// Tgl  	  : 14-12-2021                                   	         //
+// Oleh		  : Leony Mona Putri Sihaloho - 2105551022		             //
+// Revisi	  : 0					                                     //
 // Nama Fungsi    : home                                                 //
 // Deskripsi      : Meminta user untuk memilih salah satu kategori yang  //
-//		    ingin dijalankan sesuai dengan kebutuhan user.       //
+//		    ingin dijalankan sesuai dengan kebutuhan user.               //
 //=======================================================================//
+
+/*************************************************************************|
+|*************   Fungsi Untuk Menampilkan Halaman Utama   ****************|
+|=========================================================================|
+|TANGGAL PEMBUATAN : 14 DESEMBER 2021                                     |
+|OLEH : DWI TIRTA WATI                                                    |
+|DESKRIPSI : REVISI FUNGSI HOME() PADA PROGRAM                            |
+|REVISI : 1                                                               |
+|DESKRIPSI : Menambahkan pilihan "akun Info" pada halama utama pada       |
+|            switch case 5, fungsi home()                                 |
+**************************************************************************/
+
 void home(){
     pilih:
-    printf ("\n");
     printf ("\t\t\t\t\t|================================================|\n");
     printf ("\t\t\t\t\t|                   HALAMAN UTAMA                |\n");
     printf ("\t\t\t\t\t|================================================|\n");
@@ -42,7 +35,8 @@ void home(){
     printf ("\t\t\t\t\t|   2   |    Pengeluaran                         |\n");
     printf ("\t\t\t\t\t|   3   |    Transaksi                           |\n");
     printf ("\t\t\t\t\t|   4   |    Target                              |\n");
-    printf ("\t\t\t\t\t|   5   |    EXIT                                |\n");
+    printf ("\t\t\t\t\t|   5   |    INFO AKUN                           |\n");
+    printf ("\t\t\t\t\t|   6   |    EXIT                                |\n");
     printf ("\t\t\t\t\t|================================================|\n"); 
     printf ("\t\t\t\t\t|================================================|\n");
     printf ("\n\t\t\t\t\t Input Pilihan Anda = ");
@@ -61,20 +55,61 @@ void home(){
             pengeluaran ();
             break;
         //pada case 3, program akan menampilkan menu transaksi dan user akan diminta untuk memilih untuk transaksi yang di lakukan oleh user.
-        case 3:
-            transaksi ();
-            break;
-        //pada case 4, program akan menampilkan menu target yang di lakukan oleh user.
+        // case 3:
+        //     transaksi ();
+        //     break;
+        // //pada case 4, program akan menampilkan menu target yang di lakukan oleh user.
         case 4:
             target ();
             break;
+        //case 4 masuk ke fungsi infouser()--> file "display.c"
         case 5:
-            exit (0);
+            infouser();
             break;
+        case 6:
+            exit (0);
+            break;    
         default:
             error_alert();
             //goto pilih; dimana syntax ini akan membawa user kembali ke menu pemilihan program untuk menginputkan pilihan yang benar, ini akan terus berulang hingga user menginputkan pilihan yang benar sesuai apa yang sudah ditampilkan pada menu pilihan.
             goto pilih;
             break;
+    }
+}
+
+void menu_masuk(){
+    int pilihanmasuk;
+
+    // Arahan yang diberikan kepada pengguna
+    arahan:
+    printf ("\n\n\t\t\t\t\t===============================================\n");
+    printf ("\t\t\t\t\t||                 MENU MASUK                ||\n");
+    printf ("\t\t\t\t\t||===========================================||\n");
+    printf ("\t\t\t\t\t||   No  |    Menu                           ||\n");
+    printf ("\t\t\t\t\t||-------|-----------------------------------||\n");
+    printf ("\t\t\t\t\t||   1   |    Masuk                          ||\n");
+    printf ("\t\t\t\t\t||   2   |    Registrasi                     ||\n");
+    printf ("\t\t\t\t\t||   3   |    Exit                           ||\n");
+    printf ("\t\t\t\t\t===============================================\n");
+    printf ("\t\t\t\t\tInput pilihan Anda = ");
+    scanf  ("%d", &pilihanmasuk);
+    system ("clear");
+
+    switch (pilihanmasuk){
+    case 1:
+        // Jika pengguna menginput angka 1 maka dipanggil fungsi masuk
+        masuk();
+        break;
+    case 2:
+        // Jika pengguna menginput angka 2 maka dipanggil fungsi registrasi
+        daftar();
+        break;
+    case 3:
+        exit(1);
+        break;
+    default:
+    	error_alert();
+        goto arahan;
+        break;
     }
 }
