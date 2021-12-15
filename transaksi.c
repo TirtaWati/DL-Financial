aebab28ecb24897979e14eb15ece30cedd0fed1c
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-16 00:37:53 +0800

    membuat file transaksi.c

---
+++ transaksi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "structure.h"
+#include "variable.h"
+#include "function.h"
+
+//define buffer_size ini untuk mendeklarasikan banyak/jumlah buffer max pada file
+#define BUFFER_SIZE 1000
+
+
+int pemasukan (){
+    
+}
+int pengeluaran (){
+
+}
+
+void target (){
+
+}
