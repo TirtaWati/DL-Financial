af2c27062c499c87f129f9fe780ed160ab530ac7
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-21 00:41:12 +0800

    Update display.c

--- display.c
+++ display.c
@@ -74,10 +74,49 @@ void infouser (void){
 			printf  ("\t\t\t\t\t------------------------------------------\n");
 			printf  ("\t\t\t\t\t\t  NAMA : %s\n", &u.nama);
 			printf  ("\t\t\t\t\t\t  EMAIL: %s\n", &u.email);
-			printf  ("\t\t\t\t\t\t  SAKU : %2.f\n", u.uang);
+			printf  ("\t\t\t\t\t\t  SAKU : %2.f\n", u.saku);
 			printf  ("\t\t\t\t\t==========================================\n");
 		}
 	}
 	fclose(log);		/*menutup kedua file dataLog.txt dan logrecord.txt*/
+	fclose(record);
+}
+
+/*************************************************************************|
+|******************      DISPLAY LAPORAN TRANSAKSI    ********************|
+|=========================================================================|
+|TANGGAL PEMBUATAN : 20 DESEMBER 2021                       			  |
+|OLEH : DWI TIRTA WATI                                      			  |
+|DESKRIPSI : Membuat fungsi laporan() untuk menampilkan transaksi yang    |
+|			 dilakukan oleh user										  |
+|REVISI : 0                                                 			  |
+|*************************************************************************/
+
+void laporan (){
+	// penggunaan file handling pada program, mendeklarasikan pointer
+	FILE *data, *record;
+	data = fopen("dataLog.txt", "r");  			// pointer data digunakan untuk membuka file dataLog.txt dengan mode read 
+	record = fopen("logrecord.txt", "r"); 		// pointer record digunakan untuk membuka file logrecord.txt dengan mode read
+
+	//membaca username yang ada di file dataLog.txt dan mencari kesamaan antar username yang ada di logrecord
+	while(fgets(&u, BUFFER_SIZE, data)){		//membaca keseluruhan isi dari dataLog.txt
+		if(strcmp(username, u.username) == 0){	//membandingkan apakah username logrecord dan username dataLog.txt sama
+			system("clear");
+			//jika kondisi terpernuhi maka ditampilkan keseluruhan transaksi yang ada dan yang sesuai dengan username
+			printf  ("\t\t\t\t\t===================================================\n");	
+			printf  ("\t\t\t\t\t            LAPORAN TRANSAKSI HARI INI			   \n");
+			printf  ("\t\t\t\t\t---------------------------------------------------\n");
+			printf  ("\t\t\t\t\t  NAMA : %s\n", &u.nama);
+			printf  ("\t\t\t\t\t  SAKU : %2.f\n", u.saku);
+			printf  ("\t\t\t\t\t---------------------------------------------------\n");
+			printf  ("\t\t\t\t\t\t  PEMASUKAN : %2.f\n", u.in.pemasukan);
+			printf  ("\t\t\t\t\t\t  KATEGORI PEMASUKAN   : %s\n\n", u.in.kategori);
+			printf  ("\t\t\t\t\t\t  PENGELUARAN : %2.f\n", u.out.pengeluaran);
+			printf  ("\t\t\t\t\t\t  KATEGORI PENGELUARAN : %s\n", u.out.kategori);
+			printf  ("\t\t\t\t\t===================================================\n");
+		}
+		
+	}
+	fclose(data);		/*menutup kedua file dataLog.txt dan logrecord.txt*/
 	fclose(record);
 }
