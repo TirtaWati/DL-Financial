617c4c1bbd14bca992cafeca010ae88e8eb06909
Dwi Tirta Wati <schtirta@gmail.com>
2021-12-26 15:44:17 +0800

    Update variable.h

--- variable.h
+++ variable.h
@@ -8,11 +8,10 @@
 // Dekalarasi variable global yang di gunakan pada program catatan keuangan ini:
 int ktgripemasukan;                   //variable ktgripemasukan    => berfungsi untuk menyimpan yang dimasukkan oleh user pada pemilihan kategori pemasukan 
 int ktgripengeluaran;                 //variable ktgripengeluaran  => berfungsi untuk menyimpan yang dimasukkan oleh user pada pemilihan kategori pengeluaran
-time_t waktuserver;                   //variavel waktuserver       => berfungsi untuk mengambil waktu dari sistem.            
+time_t curtime;                       //variavel curtime           => berfungsi untuk mengambil waktu dari sistem.            
 char namaFile[] = "dataLog.txt";      //assign namaFile (nF) agar menyimpan string "dataLog.txt"
 int pilihanmasuk;					  //variabel pilihanmasuk      => berfungsi untuk menyimpan pilihan yang dimasukan oleh user untuk di proses ke switch case pada pemilihan menu login
 int menuhome;						  //variabel pilihanmenu       => berfungsi untuk menyimpan pilihan yang dimasukan oleh user untuk di proses ke switch case pada Halaman Utama atau Home
 char yn;                              //untuk menampung karakter input pada pilihan (Y|N) --> file "input.c (login())"
 char username[10], password[8];       //untuk menampung array of string username dan password pada login --> file "input.c" fungsi masuk()
-
 #endif
