6f9ba7f0c97a5a63b8ee41bd8ba67d1a2499573e
Dwi Tirta Wati <schtirta@gmail.com>
2022-01-05 04:49:39 +0800

    membuat fungsi laporan bulanan dan file memory untuk riwayat transaksi

--- display.c
+++ display.c
@@ -56,10 +56,10 @@ void error_alert(){
 // Deskripsi      : Fungsi waktu() ini digunakan untuk mengambil dan     //
 //                  menampilkan waktu sistem saat user mengakses program.//
 //=======================================================================//
-void waktu(){
+void waktusrvr(){
     time( &waktuserver);
     struct tm * waktu = localtime( & waktuserver);
-    printf("\n\t\t\t\t\t||================Tanggal: %i/%i/%i==============||\n\n", waktu -> tm_mday, waktu -> tm_mon + 1, waktu -> tm_year + 1900);
+    printf("\n\t\t\t\t\t||=============================Tanggal: %i/%i/%i================================||\n\n", waktu -> tm_mday, waktu -> tm_mon + 1, waktu -> tm_year + 1900);
 }
 
 
@@ -148,14 +148,34 @@ void infouser (void){
 //                  pemasukan dan pengeluaran sebelumnya.                //
 //=======================================================================//
 
+/*************************************************************************|
+|******************      DISPLAY LAPORAN TRANSAKSI   *********************|
+|=========================================================================|
+|TANGGAL PEMBUATAN : 2 JANUARI 2022                                       |
+|OLEH : DWI TIRTA WATI                                                    |
+|DESKRIPSI : REVISI FUNGSI LAPORAN() UNTUK TAMPILAN LAPORAN TRANSAKSI     |
+|REVISI : 2                                                               |
+|DESKRIPSI : Menambahkan file memory baru "transaksi.txt" untuk menyimpan |
+|            hasil laporan transaksi agar dapat digunakan pada fungsi     |
+|            lap_bulan()						  |						  |
+**************************************************************************/
+
 void laporan (){
-	time( & waktuserver);
-    struct tm * waktu = localtime( & waktuserver);																											// penggunaan file handling pada program, mendeklarasikan pointer
+	time_t t;
+
+    time(&t);
+    struct tm*n  = localtime(&t);
+        waktu.bulan = n ->tm_mon + 1;
+        waktu.tgl = n ->tm_mday;
+        waktu.tahun = n ->tm_year + 1900;
+
 	// penggunaan file handling pada program, mendeklarasikan pointer
-	FILE *data, *record;
-	data = fopen("dataLog.txt", "r");  			// pointer data digunakan untuk membuka file dataLog.txt dengan mode read 
-	record = fopen("logrecord.txt", "r"); 		// pointer record digunakan untuk membuka file logrecord.txt dengan mode read
-	
+	FILE *data, *record, *trans;
+	data = fopen("dataLog.txt", "r");  																			// pointer data digunakan untuk membuka file dataLog.txt dengan mode read 
+	record = fopen("logrecord.txt", "r"); 																		// pointer record digunakan untuk membuka file logrecord.txt dengan mode read
+	trans = fopen("transaski.txt", "a");
+
+
 	//membaca username yang ada di file dataLog.txt dan mencari kesamaan antar username yang ada di logrecord
 	while(fgets(&u, BUFFER_SIZE, data)){																	// membaca keseluruhan isi dari dataLog.txt
 		if(strcmp(username, u.username) == 0){																		// membandingkan apakah username logrecord dan username dataLog.txt sama
@@ -163,13 +183,78 @@ void laporan (){
 			printf ("\t\t========================================================================================\n");
 			printf ("\t\t                                        PEMASUKAN                                       \n");
 			printf ("\t\t========================================================================================\n");
-			printf ("\t\t|     Tanggal     |     Nama     |     Pemasukan     |     Kategori     |Jumlah Saku\n");
-			printf ("\t\t|%i-%i-%i          %s          Rp%2.f           %s               Rp%2.f\n", waktu -> tm_mday, waktu -> tm_mon + 1, waktu -> tm_year + 1900, &u.nama, u.pemasukan, u.kategori_in, u.saku);
+			printf ("\t\t|     Nama     		|     Pemasukan     |        Kategori  		|	Jumlah Saku	   	 \n");
+			printf ("\t\t|%s             		Rp%2.f             %s                 	  Rp%2.f			 \n", &u.nama, u.pemasukan, u.kategori_in, u.saku);
 			printf ("\t\t========================================================================================\n");
 			printf ("\t\t\n                                     PENGELUARAN                                      \n");
 			printf ("\t\t========================================================================================\n");
-			printf ("\t\t|     Tanggal     |     Nama     |     Pengeluaran   |     Kategori     |Jumlah Saku\n");
-			printf ("\t\t|%i-%i-%i          %s          Rp%2.f          %s               Rp%2.f\n", waktu -> tm_mday, waktu -> tm_mon + 1, waktu -> tm_year + 1900, &u.nama, u.pengeluaran, u.kategori_out, u.saku);
+			printf ("\t\t|     Nama     		|     Pengeluaran   |        Kategori  		|	Jumlah Saku	 \n");
+			printf ("\t\t|%s          			Rp%2.f             %s               	  Rp%2.f				 \n", &u.nama, u.pengeluaran, u.kategori_out, u.saku);
+			printf ("\t\t|=======================================================================================|\n");
+			//jika kondisi terpernuhi maka ditampilkan keseluruhan transaksi yang ada dan yang sesuai dengan username
+			
+			printf  ("\t\t=======================================================================================\n");
+			printf  ("\t\t\t\t\t\t     Tekan ENTER untuk kembali Halaman Utama	   \n");
+			printf  ("\t\t=======================================================================================\n");
+			getchar();
+			getchar();
+
+			// menyimpan di file transaksi.txt
+			fprintf (trans, "\n\t\tFOR USERNAME: %s																 		 \n", &u.username);
+			fprintf (trans, "\t\t========================================================================================\n");
+			fprintf (trans, "\t\t                                        PEMASUKAN                                       \n");
+			fprintf (trans, "\t\t========================================================================================\n");
+			fprintf (trans, "\t\t|     Tanggal     |     Nama     |     Pemasukan     |     Kategori     |Jumlah Saku	 \n");
+			fprintf (trans, "\t\t|%02i/%02i/%i          %s          Rp%2.f           %s               Rp%2.f			 \n", waktu.bulan, waktu.tgl, waktu.tahun, &u.nama, u.pemasukan, u.kategori_in, u.saku);
+			fprintf (trans, "\t\t========================================================================================\n");
+			fprintf (trans, "\t\t\n                                     PENGELUARAN                                      \n");
+			fprintf (trans, "\t\t========================================================================================\n");
+			fprintf (trans, "\t\t|     Tanggal     |     Nama     |     Pengeluaran   |     Kategori     |Jumlah Saku	 \n");
+			fprintf (trans, "\t\t|%02i/%02i/%i          %s          Rp%2.f          %s               Rp%2.f				 \n", waktu.bulan, waktu.tgl, waktu.tahun, &u.nama, u.pengeluaran, u.kategori_out, u.saku);
+			fprintf (trans, "\t\t|=======================================================================================|\n\n");
+		}
+		
+	}
+	fclose(data);		/*menutup file dataLog.txt dan logrecord.txt*/
+	fclose(record);
+	fclose(trans);
+	system("clear");
+	home();
+}
+
+/*************************************************************************|
+|******************      DISPLAY LAPORAN BULANAN     *********************|
+|=========================================================================|
+|TANGGAL PEMBUATAN : 3 Januari 2022                                       |
+|OLEH : DWI TIRTA WATI                                                    |
+|DESKRIPSI : Membuat fungsi lap_bulan() untuk menampilkan laporan sesuai   |
+|			 dengan inputan bulan dari user								  |
+|REVISI : 0                                                               |
+**************************************************************************/
+
+void lap_bulan(){
+	int bulan;
+	FILE *trans, *record;
+	char buff[BUFFER_SIZE];
+	trans = fopen("transaski.txt", "r");
+	record = fopen("logrecord.txt", "r");
+	printf("masukan bulan: ");
+	scanf("%i", &bulan);
+
+	while(fgets(&buff, BUFFER_SIZE, trans)){
+		if(strcmp(&u.username, &username)== 0 && waktu.bulan == bulan){
+			system("clear");
+
+			printf ("\t\t========================================================================================\n");
+			printf ("\t\t                                        PEMASUKAN                                       \n");
+			printf ("\t\t========================================================================================\n");
+			printf ("\t\t|     Tanggal     |     Nama     |     Pemasukan     |     Kategori     |Jumlah Saku	 \n");
+			printf ("\t\t|%02i/%02i/%i          %s          Rp%2.f           %s               Rp%2.f			 \n", waktu.bulan, waktu.tgl, waktu.tahun, &u.nama, u.pemasukan, u.kategori_in, u.saku);
+			printf ("\t\t========================================================================================\n");
+			printf ("\t\t\n                                     PENGELUARAN                                      \n");
+			printf ("\t\t========================================================================================\n");
+			printf ("\t\t|     Tanggal     |     Nama     |     Pengeluaran   |     Kategori     |Jumlah Saku	 \n");
+			printf ("\t\t|%02i/%02i/%i          %s          Rp%2.f          %s               Rp%2.f				 \n", waktu.bulan, waktu.tgl, waktu.tahun, &u.nama, u.pengeluaran, u.kategori_out, u.saku);
 			printf ("\t\t|=======================================================================================|\n");
 			//jika kondisi terpernuhi maka ditampilkan keseluruhan transaksi yang ada dan yang sesuai dengan username
 			
@@ -178,12 +263,22 @@ void laporan (){
 			printf  ("\t\t\t\t\t===================================================\n");
 			getchar();
 			getchar();
+			menu_laporan();
 		}
-		
+		else{
+			printf  ("\t\t\t\t\t===================================================\n");
+			printf  ("\t\t\t			Riwayat Tidak Ditemukan!			   \n");
+			printf  ("\t\t\t\t\t===================================================\n");
+			getchar();
+			getchar();
+			menu_laporan();
+		}
 	}
-	fclose(data);		/*menutup file dataLog.txt dan logrecord.txt*/
-	fclose(record);
-	system("clear");
-	home();
+	fclose(trans);
+
+
 }
+
+
+
 
